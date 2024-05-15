0a401323e56434b6f9c4c632b77f9a60f1a5908f
Puppuccino <97849040+CrazyDubya@users.noreply.github.com>
2024-05-14 21:18:47 -0400

    Create result_aggregation_system.py

---
+++ Agent_Gen_ECS/project_root/result_aggregation_system.py
@@ -0,0 +1,7 @@
+from typing import Dict
+from agent_management import AgentComponent
+
+class ResultAggregationSystem:
+    def aggregate_results(self, results: Dict[AgentComponent, str]) -> str:
+        final_result = "\n".join(results.values())
+        return final_result
